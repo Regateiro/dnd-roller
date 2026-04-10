3fbe3b8e939ce1cf73dc3cbab48e5f0bda289f11
Diogo Regateiro <diogo.regateiro@atlar.pt>
2026-04-10 14:27:58 +0100

    Update logic.

--- bot/handlers/roll.py
+++ bot/handlers/roll.py
@@ -77,6 +77,7 @@ class RollHandler:
     ) -> str:
         """Resolve the roll expression, handling direct dice rolls vs character rolls."""
         try:
-            return await self._roll_service.resolve_references(character, fields[2])
+            resolved_expr = await self._roll_service.resolve_references(character, fields[2])
+            return await self._roll_service.get_character_roll(character, resolved_expr, modifiers)
         except d20.RollError:
             return await self._roll_service.get_character_roll(character, fields[2], modifiers)
